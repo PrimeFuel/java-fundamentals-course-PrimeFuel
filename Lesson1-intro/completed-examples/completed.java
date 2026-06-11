418a74f3977a43c72595541855901bb55408febb
Diego Herrera <u202319027@upc.edu.pe>
2026-06-10 20:42:21 -0500

    docs(lesson1): add starter files.

---
+++ Lesson1-intro/completed-examples/completed.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hola Soy Diego");
+        System.out.println("Estoy aprendiendo a usar Java");
+        System.out.println("Y ver qué puede hacer.");
+
+    }
+
+}
