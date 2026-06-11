701cd1163ed3c28f4338654ae10472d01bb4745d
Diego Herrera <u202319027@upc.edu.pe>
2026-06-10 20:54:23 -0500

    docs(lesson1): update the starter file.

--- Lesson1-intro/starter-files/starter.java
+++ Lesson1-intro/starter-files/starter.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hola Mundo");
+        // Coloca el texto que desees usando System.out.println("...");
 
     }
 
